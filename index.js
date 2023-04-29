abf8b86e656e07e1801def1243ffc801cf5f8909
armando colina <armandoc26@armandos-MacBook-Air.local>
2023-04-29 19:28:59 -0400

    agregamos variables de entorno

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("Starting");
+const http = require("http");
+require('dotenv').config();
 
+function requestController() {
+    // logica de nuestra funcion
+    console.log("hola mundo !!!!!");
+}
+
+//configurar nuestro servidor 
+const server = http.createServer(requestController);
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, function () {
+    console.log('Aplicacion corriendo en puerto: ' + PORT);
+})
