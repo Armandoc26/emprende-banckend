3843448319f82b0446e6c888ab6a9e5df45e9399
armando colina <armandoc26@armandos-MacBook-Air.local>
2023-04-29 20:48:13 -0400

    agregamos la carpeta public de nuestro proyecto y y le agregamos una logica mas compleja a nuestro servidor

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const http = require("http");
 require('dotenv').config();
 
-function requestController() {
+function requestController(req, res) {
     // logica de nuestra funcion
-    console.log("hola mundo !!!!!");
+    const url = req.url;
+    const method = req.method;
+    console.log({url, method});
+
+    if (method === 'GET' && url === '/') {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        fs.readFile('./public/index.html', function(err, file) {
+            if (err) {
+                console.log('HUBO UN ERROR');
+            }
+            res.write(file);
+            res.end();
+        })
+        return;
+    }
+
+    if (method === 'GET' && url === '/about') {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        fs.readFile('./public/about.html', function(err, file) {
+            if (err) {
+                console.log('HUBO UN ERROR');
+            }
+            res.write(file);
+            res.end();
+        })
+        return;
+    } else {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.write('<h1>Pagina no encontrada</h1>');
+        res.end();
+    }
 }
 
 //configurar nuestro servidor 
